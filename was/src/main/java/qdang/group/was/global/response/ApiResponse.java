683eaa1681d59d8d1280560dee6ee82f1bcc99e3
유서린 <sprout3082@gmail.com>
2023-03-10 19:08:32 +0900

    [init] : response 추가

--- was/src/main/java/qdang/group/was/global/response/ApiResponse.java
+++ was/src/main/java/qdang/group/was/global/response/ApiResponse.java
@@ -1,2 +1,35 @@
-package qdang.group.was.global.response;public class ApiResponse {
+package qdang.group.was.global.response;
+
+import org.springframework.http.ResponseEntity;
+import qdang.group.was.global.exception.ErrorType;
+
+public class ApiResponse {
+
+    public static ResponseEntity<?> success(SuccessType successType){
+        JsonResponse<Object> response = new JsonResponse<>(successType.getHttpStatusCode(), successType.getMessage());
+        return ResponseEntity
+                .status(successType.getHttpStatusCode())
+                .body(response);
+    }
+
+    public static <T> ResponseEntity<?> success(SuccessType successType, T data){
+        JsonResponse<T> response = new JsonResponse<>(successType.getHttpStatusCode(), successType.getMessage(), data);
+        return ResponseEntity
+                .status(successType.getHttpStatusCode())
+                .body(response);
+    }
+
+    public static ResponseEntity<?> error(ErrorType errorType) {
+        JsonResponse<Object> response = new JsonResponse<>(errorType.getHttpStatusCode(), errorType.getMessage());
+        return ResponseEntity
+                .status(errorType.getHttpStatusCode())
+                .body(response);
+    }
+
+    public static ResponseEntity<?> error(ErrorType errorType, String message) {
+        JsonResponse<Object> response = new JsonResponse<>(errorType.getHttpStatusCode(), message);
+        return ResponseEntity
+                .status(errorType.getHttpStatusCode())
+                .body(response);
+    }
 }
--- was/src/main/java/qdang/group/was/global/response/JsonResponse.java
+++ was/src/main/java/qdang/group/was/global/response/JsonResponse.java
@@ -1,2 +1,15 @@
-package qdang.group.was.global.response;public class JsonResponse {
+package qdang.group.was.global.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class JsonResponse<T> {
+
+    private final int code;
+    private final String message;
+    private T data;
 }
