a231ecac11dbfc8cd8d63613c8999ab2a31746db
유서린 <sprout3082@gmail.com>
2023-05-03 22:58:50 +0900

    [Feat]: Project Init

---
+++ was/src/main/java/com/q_dang/was/WasApplication.java
@@ -0,0 +1,13 @@
+package com.q_dang.was;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasApplication.class, args);
+	}
+
+}
---
+++ was/src/test/java/com/q_dang/was/WasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.q_dang.was;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
