81dc7c7a7d97282a6c4324893119467b35df9564
유서린 <sprout3082@gmail.com>
2023-05-04 01:34:57 +0900

    [Test]: Test Controller 작성

---
+++ was/src/main/java/com/q_dang/was/TestController.java
@@ -0,0 +1,19 @@
+package com.q_dang.was;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("")
+    public String test1() {
+        return "test1";
+    }
+
+    @RequestMapping("/test")
+    public String test2() {
+        return "test2";
+    }
+
+}
