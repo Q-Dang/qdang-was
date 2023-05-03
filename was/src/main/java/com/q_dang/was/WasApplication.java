9017004ee18d5af20a31c71b3c5c687419a503ab
유서린 <sprout3082@gmail.com>
2023-05-04 01:51:17 +0900

    [Init]: WAS 파일 오류 수정

--- was/src/main/java/com/q_dang/was/WasApplication.java
+++ was/src/main/java/com/q_dang/was/WasApplication.java
@@ -2,12 +2,18 @@ package com.q_dang.was;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WasApplication {
+public class WasApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WasApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WasApplication.class);
+	}
 }
