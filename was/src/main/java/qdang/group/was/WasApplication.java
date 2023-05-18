c8a9700fcd25f8d32a035a26bb21b7e6d9b6366c
유서린 <sprout3082@gmail.com>
2023-05-18 18:25:04 +0900

    [Fix] : .war 파일 변환

---
+++ was/src/main/java/qdang/group/was/WasApplication.java
@@ -0,0 +1,13 @@
+package qdang.group.was;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasApplication.class, args);
+	}
+
+}
---
+++ was/src/test/java/qdang/group/was/WasApplicationTests.java
@@ -0,0 +1,13 @@
+package qdang.group.was;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
