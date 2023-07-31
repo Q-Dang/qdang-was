577e30f0a610b53dc28c5aba55b42b4e1595a704
유서린 <sprout3082@gmail.com>
2023-07-31 13:41:26 +0900

    #1 [feat]  유저 load port 수정

--- application/src/main/java/com/qdang/application/user/port/out/LoadUserPort.java
+++ application/src/main/java/com/qdang/application/user/port/out/LoadUserPort.java
@@ -1,9 +1,16 @@
 package com.qdang.application.user.port.out;
 
+import com.qdang.application.match.domain.Match;
 import com.qdang.application.user.domain.User;
+import java.util.List;
+
 public interface LoadUserPort {
 
 	User loadById(Long userId);
+
 	User loadByUsername(String username);
+
 	User loadByLoginId(String loginId);
+
+	List<User> loadAllByMatchId(Long matchId);
 }
