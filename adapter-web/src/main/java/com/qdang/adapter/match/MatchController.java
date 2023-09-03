a1a5bc9ab6a6e74ea2dc09f4d091722e0d724bd0
유서린 <sprout3082@gmail.com>
2023-09-03 18:34:40 +0900

    #9 [feat] 경기 상세조회 작업 중..

--- adapter-web/src/main/java/com/qdang/adapter/match/MatchController.java
+++ adapter-web/src/main/java/com/qdang/adapter/match/MatchController.java
@@ -2,8 +2,9 @@ package com.qdang.adapter.match;
 
 import com.qdang.adapter.match.request.QuitMatchRequest;
 import com.qdang.adapter.match.request.RecordMatchProcessRequest;
+import com.qdang.adapter.match.response.GetMatchInfoRequest;
 import com.qdang.adapter.match.response.StartMatchResponse;
-import com.qdang.application.match.domain.Match;
+import com.qdang.application.match.port.in.GetMatchUseCase;
 import com.qdang.application.match.port.in.QuitGameUseCase;
 import com.qdang.application.match.port.in.RecordMatchProcessUseCase;
 import com.qdang.application.match.port.in.StartMatchUseCase;
@@ -19,11 +20,15 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @V1
 @WebAdapter
 @RequiredArgsConstructor
@@ -32,9 +37,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Tag(name = "Match", description = "Match API Document")
 public class MatchController {
 
+	private final GetMatchUseCase getMatchUseCase;
 	private final StartMatchUseCase startMatchUseCase;
 	private final RecordMatchProcessUseCase recordMatchProcessUseCase;
 	private final QuitGameUseCase quitGameUseCase;
+
+	@Operation(summary = "게임 상세 조회")
+	@ApiResponse(
+			responseCode = "200",
+			description = "게임 상세 조회 성공")
+	@GetMapping("/{matchId}")
+	public ResponseEntity<GetMatchInfoRequest> getMatchInfo(
+			@PathVariable Long matchId
+	) {
+		GetMatchInfoRequest response =
+				GetMatchInfoRequest.from(
+						getMatchUseCase.getMatchByMatchId(matchId));
+		return HttpResponse.success(
+				SuccessType.READ_RESOURCE_SUCCESS,
+				response);
+	}
 
 	@Operation(summary = "게임 생성 및 매칭 시작")
 	@ApiResponse(
@@ -45,10 +67,12 @@ public class MatchController {
 			@UserId Long userId,
 			@Valid @RequestBody StartMatchRequest request
 	) {
-		Match match = startMatchUseCase.startMatch(request.toStartMatchCommand());
+		StartMatchResponse response =
+				StartMatchResponse.from(
+						startMatchUseCase.startMatch(request.toStartMatchCommand()));
 		return HttpResponse.success(
 				SuccessType.CREATE_RESOURCE_SUCCESS,
-				StartMatchResponse.from(match));
+				response);
 	}
 
 	@Operation(summary = "턴 기록")
