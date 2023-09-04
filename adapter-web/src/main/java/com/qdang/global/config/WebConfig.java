3085a8f67592c2750b07738919cfd48c0effed74
유서린 <sprout3082@gmail.com>
2023-09-05 01:30:04 +0900

    #1 [refactor] aspect annotation 이동

--- adapter-web/src/main/java/com/qdang/global/config/WebConfig.java
+++ adapter-web/src/main/java/com/qdang/global/config/WebConfig.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.validation.Validator;
@@ -19,6 +20,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.UrlPathHelper;
 
 @Configuration
+@EnableAspectJAutoProxy
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
