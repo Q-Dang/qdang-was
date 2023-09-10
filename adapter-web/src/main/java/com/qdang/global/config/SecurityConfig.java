18b016f3c8646fbfe3464cc0cff54ac3fc47dda3
유서린 <sprout3082@gmail.com>
2023-09-10 15:46:32 +0900

    #1 [feat] spring security config 수정

--- adapter-web/src/main/java/com/qdang/global/config/SecurityConfig.java
+++ adapter-web/src/main/java/com/qdang/global/config/SecurityConfig.java
@@ -2,27 +2,40 @@ package com.qdang.global.config;
 
 import com.qdang.global.filter.JwtAuthenticationFilter;
 import com.qdang.global.filter.JwtExceptionFilter;
+import com.qdang.global.handler.JwtAccessDeniedHandler;
 import com.qdang.global.http.HeaderTokenExtractor;
 import com.qdang.global.jwt.JwtResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-	private final JwtResolver jwtResolver;
+	private final JwtExceptionFilter jwtExceptionFilter;
+	private final JwtAccessDeniedHandler customAccessDeniedHandler;
 	private final HeaderTokenExtractor headerTokenExtractor;
-//	private JwtExceptionFilter jwtExceptionFilter;
-//	private JwtAuthenticationFilter jwtAuthenticationFilter;
+	private final JwtResolver jwtResolver;
+
+	private static final String[] SwaggerPatterns = {
+			"/swagger-resources/**",
+			"/swagger-ui/**",
+			"/v3/api-docs/**",
+			"/api-docs/**",
+			"/webjars/**",
+	};
 
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
@@ -31,40 +44,48 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		JwtExceptionFilter jwtExceptionFilter =
-				new JwtExceptionFilter();
 		JwtAuthenticationFilter jwtAuthenticationFilter =
 				new JwtAuthenticationFilter(headerTokenExtractor, jwtResolver);
-
-		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-		http.addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class);
+		http
+				.addFilterBefore(jwtAuthenticationFilter,
+						UsernamePasswordAuthenticationFilter.class)
+				.addFilterBefore(jwtExceptionFilter,
+						JwtAuthenticationFilter.class)
+				.exceptionHandling()
+				.accessDeniedHandler(customAccessDeniedHandler);
 
 		http.httpBasic().disable();
 		http.csrf().disable();
 		http.cors().disable();
 
-//		http.sessionManagement()
-//				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-		http.formLogin().disable();
-
-		http.authorizeRequests()
-				.antMatchers(("/manager/**"))
-				.access("hasAuthority('MANAGER') or hasAuthority('ADMIN')")
-//				.hasAuthority("MANAGER")
-				.antMatchers(("/admin/**"))
-				.access("hasAuthority('ADMIN')")
-				.anyRequest().authenticated();
+		http.authorizeHttpRequests()
+				.requestMatchers(new AntPathRequestMatcher("/manager/**"))
+				.hasAnyAuthority("MANAGER", "ADMIN")
+				.requestMatchers(new AntPathRequestMatcher("/admin/**"))
+				.hasAuthority("ADMIN")
+				.anyRequest()
+				.authenticated();
 		return http.build();
 	}
 
 	@Bean
-	public WebSecurityCustomizer webSecurityCustomizer(){
+	public WebSecurityCustomizer webSecurityCustomizer() {
 		return web -> {
 			web.ignoring()
-					.requestMatchers(PathRequest.toStaticResources().atCommonLocations())
-					.antMatchers("/v1/auth/login");
+					.antMatchers(SwaggerPatterns)
+					.antMatchers("/actuator/**")
+					.requestMatchers(
+			PathRequest.toStaticResources().atCommonLocations())
+					.requestMatchers(
+							new AntPathRequestMatcher("/v1/auth/login", HttpMethod.POST.name()),
+							new AntPathRequestMatcher("/v1/auth/signup", HttpMethod.POST.name()),
+							new AntPathRequestMatcher("/v1/matches/{matchId}", HttpMethod.GET.name()),
+							new AntPathRequestMatcher("/v1/users/search", HttpMethod.GET.name()),
+							new AntPathRequestMatcher("/v1/users/validation/username", HttpMethod.GET.name())
+					);
 		};
 	}
-
 }
