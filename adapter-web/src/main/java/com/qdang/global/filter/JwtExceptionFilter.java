fb42cdaf47f6a7e2aeac6b2e25ef77c72d0e5485
유서린 <sprout3082@gmail.com>
2023-09-10 15:32:38 +0900

    #1 [fix] filter  error fix

--- adapter-web/src/main/java/com/qdang/global/filter/JwtExceptionFilter.java
+++ adapter-web/src/main/java/com/qdang/global/filter/JwtExceptionFilter.java
@@ -12,13 +12,15 @@ import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Slf4j
+@Component
 @RequiredArgsConstructor
 public class JwtExceptionFilter extends OncePerRequestFilter {
 
-	private final ObjectMapper objectMapper = new ObjectMapper();
+	private final ObjectMapper objectMapper;
 
 	@Override
 	protected void doFilterInternal(
