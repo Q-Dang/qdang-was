36c64a098da6394dcfeeca8bef19a22cf76e268b
유서린 <sprout3082@gmail.com>
2023-10-06 23:34:01 +0900

    #11 [feat] add PostLikes domain

--- application/src/main/java/com/qdang/application/noticeboard/domain/PostLikes.java
+++ application/src/main/java/com/qdang/application/noticeboard/domain/PostLikes.java
@@ -1,2 +1,16 @@
-package com.qdang.application.noticeboard.domain;public class PostLikes {
+package com.qdang.application.noticeboard.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class PostLikes {
+
+	private Long id;
+	private Long postId;
+	private Long userId;
 }
