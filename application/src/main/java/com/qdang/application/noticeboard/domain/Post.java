781c04e3663da94df462ee79bd34424d93969c61
유서린 <sprout3082@gmail.com>
2023-10-06 23:33:50 +0900

    #11 [feat] add post domain

--- application/src/main/java/com/qdang/application/noticeboard/domain/Post.java
+++ application/src/main/java/com/qdang/application/noticeboard/domain/Post.java
@@ -1,2 +1,23 @@
-package com.qdang.application.noticeboard.domain;public class Post {
+package com.qdang.application.noticeboard.domain;
+
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Post {
+
+	private Long id;
+	private Long noticeBoardId;
+	private Long userId;
+	private Boolean isAnonymous;
+	private String title;
+	private String content;
+	private Boolean isDeleted;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
 }
