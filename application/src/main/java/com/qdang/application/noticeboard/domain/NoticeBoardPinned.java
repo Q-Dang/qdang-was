a95aa17005cd725ac0aa3154e22ba3dfe382b845
유서린 <sprout3082@gmail.com>
2023-10-06 23:32:18 +0900

    #11 [feat] add NoticeBoardPinned doamin

--- application/src/main/java/com/qdang/application/noticeboard/domain/NoticeBoardPinned.java
+++ application/src/main/java/com/qdang/application/noticeboard/domain/NoticeBoardPinned.java
@@ -1,2 +1,25 @@
-package com.qdang.application.noticeboard.domain;public class NoticeBoardPinned {
+package com.qdang.application.noticeboard.domain;
+
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class NoticeBoardPinned {
+
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "notice_board_id", nullable = false)
+	private Long noticeBoardId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private Long userId;
 }
