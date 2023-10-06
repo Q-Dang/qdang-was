4ebddcf3fe79d3426be34c5cc272897961a9805a
유서린 <sprout3082@gmail.com>
2023-10-06 23:32:53 +0900

    #11 [feat] add Scrap domain

--- application/src/main/java/com/qdang/application/noticeboard/domain/Scrap.java
+++ application/src/main/java/com/qdang/application/noticeboard/domain/Scrap.java
@@ -1,2 +1,25 @@
-package com.qdang.application.noticeboard.domain;public class Scrap {
+package com.qdang.application.noticeboard.domain;
+
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Scrap {
+
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "post_id", nullable = false)
+	private Long postId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private Long userId;
 }
