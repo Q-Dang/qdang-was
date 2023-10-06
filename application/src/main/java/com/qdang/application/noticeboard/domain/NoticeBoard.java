0b324a87fe7430713f11ab8b7b122853332686f6
유서린 <sprout3082@gmail.com>
2023-10-06 23:32:05 +0900

    #11 [feat] add NoticeBoard domain

--- application/src/main/java/com/qdang/application/noticeboard/domain/NoticeBoard.java
+++ application/src/main/java/com/qdang/application/noticeboard/domain/NoticeBoard.java
@@ -1,2 +1,23 @@
-package com.qdang.application.noticeboard.domain;public class NoticeBoard {
+package com.qdang.application.noticeboard.domain;
+
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class NoticeBoard {
+
+	private Long id;
+	private String boardName;
+	private Boolean anonymousPossible;
+	private Boolean isAdminPossible;
+	private Boolean isManagerPossible;
+	private Boolean isMemberPossible;
+	private Boolean isDeleted;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
 }
