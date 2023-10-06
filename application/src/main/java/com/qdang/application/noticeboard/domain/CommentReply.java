9e3aa053d3de84cb4d620dc28b7c290fd98b091c
유서린 <sprout3082@gmail.com>
2023-10-06 23:28:56 +0900

    #11 [feat] add CommentReply domain

--- application/src/main/java/com/qdang/application/noticeboard/domain/CommentReply.java
+++ application/src/main/java/com/qdang/application/noticeboard/domain/CommentReply.java
@@ -1,2 +1,20 @@
-package com.qdang.application.noticeboard.domain;public class CommentReply {
+package com.qdang.application.noticeboard.domain;
+
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class CommentReply {
+
+	private Long id;
+	private Long commentId;
+	private Long userId;
+	private Boolean isDeleted;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
 }
