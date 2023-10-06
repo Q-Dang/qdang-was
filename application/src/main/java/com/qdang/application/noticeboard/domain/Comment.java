15c87b0dadcad4ee3dc3a430ec379c7f357f448c
유서린 <sprout3082@gmail.com>
2023-10-06 23:27:12 +0900

    #11 [feat] add Comment domain

--- application/src/main/java/com/qdang/application/noticeboard/domain/Comment.java
+++ application/src/main/java/com/qdang/application/noticeboard/domain/Comment.java
@@ -1,2 +1,20 @@
-package com.qdang.application.noticeboard.domain;public class Comment {
+package com.qdang.application.noticeboard.domain;
+
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Comment {
+	private Long id;
+	private Long userId;
+	private Long postId;
+	private String content;
+	private Boolean isDeleted;
+	protected LocalDateTime createdAt;
+	protected LocalDateTime updatedAt;
 }
