a9b54cf88578e71e001a91f2ee4f56fcba37d68a
유서린 <sprout3082@gmail.com>
2023-10-10 13:19:44 +0900

    #5 [rename] TokenResponse 폴더 이동

--- adapter-web/src/main/java/com/qdang/adapter/auth/response/TokenResponse.java
+++ adapter-web/src/main/java/com/qdang/adapter/auth/response/TokenResponse.java
@@ -1,4 +1,4 @@
-package com.qdang.adapter.user.response;
+package com.qdang.adapter.auth.response;
 
 import com.qdang.application.user.domain.TokenCollection;
 import io.swagger.v3.oas.annotations.media.Schema;
