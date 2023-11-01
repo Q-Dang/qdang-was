28a569d1f7da6e46af89e79b39592cc3d3dcbe5e
유서린 <sprout3082@gmail.com>
2023-11-02 02:29:47 +0900

    #1 [feat] refactoring redis - template + cache

---
+++ application/src/main/java/com/qdang/global/config/RedisConfig.java
@@ -0,0 +1,74 @@
+package com.qdang.global.config;
+
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableRedisRepositories
+@EnableCaching
+public class RedisConfig {
+
+	private final RedisProperties redisProperties;
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		return new LettuceConnectionFactory(
+				redisProperties.getHost(),
+				redisProperties.getPort());
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
+		return redisTemplate;
+	}
+
+	@Bean
+	public RedisCacheConfiguration redisCacheConfiguration() {
+		return RedisCacheConfiguration.defaultCacheConfig()
+				.entryTtl(Duration.ofSeconds(60))
+				.disableCachingNullValues()
+				.serializeKeysWith(
+						RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer())
+				)
+				.serializeValuesWith(
+						RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())
+				);
+	}
+
+	//
+	@Bean
+	public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+		return (builder) -> builder
+				.withCacheConfiguration("noticeBoard",
+						RedisCacheConfiguration.defaultCacheConfig()
+								.entryTtl(Duration.ofHours(12))
+								.computePrefixWith(cacheName -> "prefix::" + cacheName + "::")
+								.disableCachingNullValues()
+								.serializeKeysWith(
+										RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer())
+								)
+								.serializeValuesWith(
+										RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())
+								))
+				.withCacheConfiguration("post",
+						RedisCacheConfiguration.defaultCacheConfig()
+								.entryTtl(Duration.ofMinutes(30))
+								.disableCachingNullValues());
+	}
+}
